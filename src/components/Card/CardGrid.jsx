5218697063af8eec7ad700519a775b5e3302a6af
bruno <brunoacy12@gmail.com>
2022-08-09 01:02:24 -0500

    Agrege funcionalidad al componente Search

--- src/components/Card/CardGrid.jsx
+++ src/components/Card/CardGrid.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import CardItem from './CardItem';
 import { Grid } from '@material-ui/core';
 import { Pagination } from '@mui/material';
@@ -10,27 +10,34 @@ import Search from '../Search';
 const CardGrid = ({ services, deleteService, editService }) => {
   const firstIndex = 0;
   const pageSize = 6;
+  const [serviceData, setServicesData] = useState(services);
   const [page, setPage] = useState(1);
-  const [data, setData] = useState(services.slice(firstIndex, pageSize));
+  const [data, setData] = useState(serviceData.slice(firstIndex, pageSize));
 
   useEffect(() => {
-    const dataFilter = services.slice(
-      firstIndex + pageSize * (page - 1),
-      pageSize * page
-    );
-    setData(dataFilter);
-  }, [services, page]);
+    setServicesData(services);
+  }, [services]);
 
-  const handleChange = (e, value) => {
+  useEffect(() => {
+    setData(serviceData.slice(firstIndex, pageSize));
+  }, [serviceData]);
+
+  useEffect(() => {
+    setData(serviceData.slice(0, pageSize));
+  }, [pageSize]);
+
+  const handleChange = (event, value) => {
     setPage(value);
     setData(
-      services.slice(firstIndex + pageSize * (value - 1), pageSize * value)
+      serviceData.slice(firstIndex + pageSize * (value - 1), pageSize * value)
     );
   };
+  console.log('sserviceData', serviceData);
+  console.log('data', data);
 
   return (
     <>
-      <Search></Search>
+      <Search services={services} setServicesData={setServicesData}></Search>
       <div className="card-grid-container">
         <Grid container spacing={4} className="card-grid">
           {data.map((service, index) => (
@@ -46,14 +53,16 @@ const CardGrid = ({ services, deleteService, editService }) => {
         </Grid>
       </div>
       <div>
-        {services.length ? (
-          <Pagination
-            count={Math.ceil(services.length / pageSize)}
-            page={page}
-            onChange={handleChange}
-            color="primary"
-            className="pagination"
-          />
+        {serviceData.length ? (
+          <>
+            <Pagination
+              count={Math.ceil(serviceData.length / pageSize)}
+              page={page}
+              onChange={handleChange}
+              color="primary"
+              className="pagination"
+            />
+          </>
         ) : (
           <AlertInfo />
         )}
--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -2,9 +2,30 @@ import React, { useState } from 'react';
 import { useDebounce } from 'react-use';
 import './index.css';
 
-const Search = () => {
+const Search = ({ services, setServicesData }) => {
   const [val, setVal] = useState('');
-  const [,] = useDebounce(() => {}, 1000, [val]);
+  const [,] = useDebounce(
+    () => {
+      SearchData(val);
+    },
+    1000,
+    [val]
+  );
+
+  const SearchData = value => {
+    const lowerCaseValue = value.toLowerCase().trim();
+    if (!lowerCaseValue) {
+      setServicesData(services);
+    } else {
+      const searchServices = services.filter(item => {
+        return Object.keys(item).some(key =>
+          item[key].toString().toLowerCase().includes(lowerCaseValue)
+        );
+      });
+      setServicesData(searchServices);
+    }
+  };
+
   return (
     <div className="search-input-container">
       <input
